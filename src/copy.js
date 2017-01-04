31df8cb4ff7dfa5b5698d3fd43934bea0f705af0
Pontus Persson <pom@skurt.com>
2017-01-04 03:01:35 -0800

    Better export.

--- src/copy.js
+++ src/copy.js
@@ -1,19 +1,28 @@
-function copy(text) {
-  var doc = document,
-      body = doc.body,
-      input = doc.createElement('input');
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(['microclip'], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.microclip = factory();
+  }
+}(this, function () {
+  function copy(text) {
+    var doc = document,
+        body = doc.body,
+        input = doc.createElement('input');
 
-  input.style.position = 'absolute';
-  input.style.top = '-100px';
-  input.value = text;
+    input.style.position = 'absolute';
+    input.style.top = '-100px';
+    input.value = text;
 
-  body.appendChild(input);
-  input.select();
-  doc.execCommand('cut');
-  input.blur();
-  body.removeChild(input);
-}
+    body.appendChild(input);
+    input.select();
+    doc.execCommand('cut');
+    body.removeChild(input);
+  }
 
-module.exports = {
-  copy,
-}
+  return {
+    copy: copy,
+  };
+}));
